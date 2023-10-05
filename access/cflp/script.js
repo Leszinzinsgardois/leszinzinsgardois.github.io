5cbff84c6dcd98c05566a2dfe66df79131091adc
Leszinzinsgardois <139361498+Leszinzinsgardois@users.noreply.github.com>
2023-10-05 15:10:28 +0000

    adding javascript code

---
+++ access/cflp/script.js
@@ -0,0 +1,9 @@
+function openPopup() {
+    const popup = document.getElementById("popup");
+    popup.style.display = "block";
+}
+
+function closePopup() {
+    const popup = document.getElementById("popup");
+    popup.style.display = "none";
+}
