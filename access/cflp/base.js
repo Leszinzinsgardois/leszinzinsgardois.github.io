2d59c54745d4c462a99c030b8c46534a337ad39a
Leszinzinsgardois <139361498+Leszinzinsgardois@users.noreply.github.com>
2023-10-05 07:16:59 +0000

    adding base

---
+++ access/cflp/base.js
@@ -0,0 +1,3 @@
+// Programmer une base de données
+// Ajouter une fonction de redirection par "referrer"
+// Lier les saisies à une "récupération" de données pour obtenir des indications sur l'appareil utilisé.
