52a1df7d09289510f77ccd2426c2b1f1229539f4
Les Zinszins Gardois <leszinzinsgardois@gmail.com>
2023-10-08 19:22:11 +0200

    adding login.js

--- access/cflp/base.js
+++
@@ -1,3 +0,0 @@
-// Programmer une base de données
-// Ajouter une fonction de redirection par "referrer"
-// Lier les saisies à une "récupération" de données pour obtenir des indications sur l'appareil utilisé.
