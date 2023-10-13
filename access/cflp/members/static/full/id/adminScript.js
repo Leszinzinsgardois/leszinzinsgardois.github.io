2ea958bf46273736b40dfbbd2bfcfa723c183ebb
Les Zinszins Gardois <leszinzinsgardois@gmail.com>
2023-10-13 09:42:20 +0200

    adding login options
    The login options have not been configurated for the moment.

--- access/cflp/members/static/full/id/adminScript.js
+++ access/cflp/members/static/full/id/adminScript.js
@@ -1,9 +1,14 @@
-const urlParams = new URLSearchParams(window.location.search);
-const passwordParam = urlParams.get('password');
-
-if (passwordParam === 'true') {
-  console.log("Mot de passe correct. Accès autorisé à la page d'administration.");
-} else {
-  console.log("Mot de passe incorrect. Redirection vers la page de mot de passe.");
-  window.location.href = 'login.html';
-}
+function displayWelcomeMessage() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const username = urlParams.get('username');
+  
+    if (!username || !validateUser(username)) {
+      redirectToLogin();
+    } else {
+      const welcomeMessage = document.getElementById('welcome-message');
+      welcomeMessage.innerText = `Bienvenue, ${username} !`;
+    }
+  }
+  
+  displayWelcomeMessage();
+  
--- access/cflp/members/static/full/id/passwordScript.js
+++ access/cflp/members/static/full/id/passwordScript.js
@@ -1,12 +1,10 @@
-const correctPassword = "cflp prevert"; // Remplacez par votre mot de passe
-
-function checkPassword() {
-  const passwordInput = document.getElementById('password').value;
-  if (passwordInput === correctPassword) {
-    // Redirection vers la page d'administration avec le bon paramètre
-    window.location.href = 'admin.html?password=true';
-  } else {
-    alert('Mot de passe incorrect. Veuillez réessayer.');
+function loginUser() {
+    const username = document.getElementById('username').value;
+  
+    if (validateUser(username)) {
+      window.location.href = `admin.html?username=${username}`;
+    } else {
+      alert('Nom d\'utilisateur incorrect. Veuillez réessayer.');
+    }
   }
-}
-
+  
