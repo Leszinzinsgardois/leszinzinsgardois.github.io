8f56fef9decabba8acc10acc03ace0aa2abdd71d
Les Zinszins Gardois <leszinzinsgardois@gmail.com>
2023-10-15 18:44:37 +0200

    adding web architecture

--- access/cflp/members/static/full/id/meta/loginScript.js
+++ access/cflp/members/static/full/id/meta/loginScript.js
@@ -1,6 +1,18 @@
 const credentials = {
   'admin': 'admin', // Compte Admin
-  '35848267': '...' // Compte Antony
+  '35848267': 'cflpprevert_fonda', // Compte Antony
+  '12482523': 'aksel_cflp_prevert', // Compte Aksel
+  '1934942': 'lina_cflp_prevert',
+  '12847351': 'aymeric_cflp_prevert',
+  '2342593586':'doryan_cflp_prevert',
+  '29349259':'katya_cflp_prevert',
+  '283845234': 'laurie_cflp_prevert',
+  '346678312': 'léane_cflp_prevert',
+  '314828512': 'maxance_cflp_prevert',
+  '8238584': 'roxane_cflp_prevert',
+  '19499425': 'tiphaine_cflp_prevert',
+  '13484251': 'camille_cflp_prevert'
+
   // Données utilisateurs ici
 };
 
@@ -14,6 +26,3 @@ function login() {
       alert('Identifiant ou mot de passe incorrect, veuillez réessayer');
   }
 }
-
-
-
